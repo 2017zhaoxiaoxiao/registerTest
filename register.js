70acde415bd731caadfed848e84473849ad1ee18
zhaoxiaoxiao <1877145940@qq.com>
2020-03-18 11:13:25 +0800

    修改

--- register.js
+++ register.js
@@ -4,17 +4,43 @@ $(function(){
         pwd=$('#pwd'),
         code=$('#code'),
         btn=$('#btn'),
-        register=$('register');
+        register=$('#register');
     register.click(function(){
-        if(!dataTest('#phone')||!dataTest('#name')||!dataTest('#pwd')||!dataTest('#code'))return ;
+
+        if(!dataTest('#name')||!dataTest('#pwd')||!dataTest('#phone')||!dataTest('#code'))
+        return ;
     })
+   function countdown(obj) {
+        if(countdownNum == 0) {
+            obj.attr('disabled', false); 
+            obj.html('获取验证码');
+            countdownNum = 10;
+            $('#code-valid').html("验证超时请稍后重试");
+            
+            return;
+        } else {
+            obj.attr('disabled', true);
+            obj.html(countdownNum + '秒');
+            countdownNum--;
+        }
+
+        setTimeout(function () {
+            countdown(obj);
+        }, 1000);  
+    }
     btn.click(function(){
         if(phone.val()===''){
             $('#phone-valid').html("请您输入手机号");
         }
+       
         else if(dataTest('#phone')){
-            $('#code-valid').html("请求超时，请稍后再试")
+         
+            countdownNum=10;
+            countdown(btn)
+           
         }
+       
+        
     })
     name.focusout(function(){
         if(!dataTest('#name')){
@@ -32,11 +58,12 @@ $(function(){
         }
       })
      
-    
+
    
 function dataTest(p){
     var data=$(p);
     if(p==='#name'){
+      
         if(!(/^[a-zA-Z0-9_\u4e00-\u9fa5]+$/.test(data.val()))){
             $(p+'-valid').html("用户名仅支持中英文、数字、下划线")
            
@@ -44,6 +71,7 @@ function dataTest(p){
         }
     }
     if(p==='#phone'){
+        
         if(!(/^1[3456789]\d{9}$/.test(data.val()))){
             $(p+'-valid').html('手机号码格式不对');
            
@@ -51,8 +79,16 @@ function dataTest(p){
         }
     }
     if(p==='#pwd'){
+       
         if(!(/(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^.{8,14}$/.test(data.val()))){
             $(p+'-valid').html("密码设置不符合要求")
+            return false;
+        }
+    }
+    if(p==='#code'){
+        if(data.val()===''){
+            console.log('1');
+            $(p+'-valid').html("验证码为空");
             return false;
         }
     }
